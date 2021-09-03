1815e47c314c5a6e62cd752a49e391d96a86524f
Chris Nelson <cnelson04@gmail.com>
2021-09-03 18:01:05 -0500

    checkboxes and button created

--- Scripts/client.js
+++ Scripts/client.js
@@ -7,7 +7,7 @@ function onReady(){
 function buildBLT (ingredients){
     console.log('in buildBLT');
     let realSandwich = false;
-    if (ingredients[0] === 'bread' && ingredients[ingredients.length-1] === 'bread' && ingredients.includes('bacon' && 'lettuce' && 'tomatoes')){
+    if (ingredients[0] === 'bread' && ingredients[ingredients.length-1] === 'bread' && ingredients.includes('bacon') && ingredients.includes('lettuce') && ingredients.includes('tomatoes')){
        realSandwich = true 
     } //  end if
     console.log(realSandwich)
@@ -18,3 +18,4 @@ buildBLT(['bread', 'lettuce', 'bacon', 'tomatoes', 'bread']) // expect true
 buildBLT(['bread', 'lettuce', 'bacon', 'tomatoes']) // expect false
 buildBLT(['lettuce', 'bacon', 'tomatoes', 'bread']) // expect false
 buildBLT(['bread', 'lettuce', 'bacon', 'tomatoes', 'celery', 'kiwi', 'kimchee','bread']) // expect true
+buildBLT(['bread', 'lettuce', 'tomatoes', 'bread']) // expect false
